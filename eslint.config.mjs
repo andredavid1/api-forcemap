3a298841d1f04cb97b842f0fa46cf724edca5e64
André David dos Santos <andredavid1@yahoo.com.br>
2025-07-22 20:59:01 -0300

    chore: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
-// @ts-check
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import prettirPlugin from "eslint-plugin-prettier";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked, // Use recommendedTypeChecked for type-aware linting
+  prettier,
   {
     languageOptions: {
       parserOptions: {
@@ -12,5 +14,13 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  }
+    plugins: {
+      prettier: prettirPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+    },
+  },
 );
