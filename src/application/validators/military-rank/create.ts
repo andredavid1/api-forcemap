9fde4f74ae08145e347361554c4c4bc3ce0fcf26
André David Dos Santos <andredavid1@yahoo.com.br>
2025-07-23 12:33:30 +0000

    refactor: improving missing field validator

--- src/application/validators/military-rank/create.ts
+++ src/application/validators/military-rank/create.ts
@@ -2,13 +2,34 @@ import { MilitaryRankProps } from "../../../domain/entities";
 import { IMilitaryRankPropsValidator } from "../../../domain/validators";
 
 export class MilitaryRankPropsValidator implements IMilitaryRankPropsValidator {
-  public readonly validateOrThrow = (props: MilitaryRankProps): void => {
-    if (!props.abbreviation) {
-      throw new Error("Campo Abreviatura precisa ser preenchido.");
+  private props: MilitaryRankProps = {} as MilitaryRankProps;
+
+  private readonly setMilitaryRankProps = (props: MilitaryRankProps): void => {
+    this.props = props;
+  };
+
+  private readonly missingFieldsValidator = () => {
+    interface RequiredFields {
+      field: keyof MilitaryRankProps;
+      label: string;
     }
 
-    if (!props.order) {
-      throw new Error("Campo Ordem precisa ser preenchido.");
+    const requiredFields: RequiredFields[] = [
+      { field: "abbreviation", label: "Abreviatura" },
+      { field: "order", label: "Ordem" },
+    ];
+
+    for (const fieldValidator of requiredFields) {
+      if (!this.props[fieldValidator.field]) {
+        throw new Error(
+          `Campo ${fieldValidator.label} precisa ser preenchido.`,
+        );
+      }
     }
+  };
+
+  public readonly validateOrThrow = (props: MilitaryRankProps): void => {
+    this.setMilitaryRankProps(props);
+    this.missingFieldsValidator();
   };
 }
