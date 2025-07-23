ef4366267a4475d0a551b8633ecbd9ca192f2aa2
André David Dos Santos <andredavid1@yahoo.com.br>
2025-07-23 11:25:42 +0000

    test: ensure that MilitaryRankPropsValidator throws if no abbreviation is provided

---
+++ src/__tests__/application/validators/military-rank/create.spec.ts
@@ -0,0 +1,31 @@
+import { MilitaryRankPropsValidator } from "../../../../application/validators/military-rank/create";
+import { MilitaryRankProps } from "../../../../domain/entities";
+import { IMilitaryRankPropsValidator } from "../../../../domain/validators";
+
+interface SutTypes {
+  sut: IMilitaryRankPropsValidator;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new MilitaryRankPropsValidator();
+
+  return { sut };
+};
+
+describe("MilitaryRankPropsValidator", () => {
+  let sutInstance: SutTypes;
+
+  beforeEach(() => {
+    sutInstance = makeSut();
+  });
+
+  it("should throw if abbreviation is not provided", () => {
+    const { sut } = sutInstance;
+
+    const props = { order: 1 } as MilitaryRankProps;
+
+    expect(() => sut.validateOrThrow(props)).toThrow(
+      "Campo Abreviatura precisa ser preenchido.",
+    );
+  });
+});
