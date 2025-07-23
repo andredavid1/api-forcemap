575206478b3c0e21682c9694b50d13465e573801
André David Dos Santos <andredavid1@yahoo.com.br>
2025-07-23 11:24:08 +0000

    feat: implements missing abbreviation validator

---
+++ src/application/validators/military-rank/create.ts
@@ -0,0 +1,10 @@
+import { MilitaryRankProps } from "../../../domain/entities";
+import { IMilitaryRankPropsValidator } from "../../../domain/validators";
+
+export class MilitaryRankPropsValidator implements IMilitaryRankPropsValidator {
+  public readonly validateOrThrow = (props: MilitaryRankProps): void => {
+    if (!props.abbreviation) {
+      throw new Error("Campo Abreviatura precisa ser preenchido.");
+    }
+  };
+}
